5b199a4fea6bdcef8b053ac392020d50eaf24c12
shawnbarron16 <shawnbarron16@gmail.com>
2021-04-26 14:00:34 -0500

    Added a message to pets

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,32 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const petClicked = clickEvent.target
+        if (petClicked.id.startsWith("pet")) {
+            const [,petId] = petClicked.id.split("--")
+
+            for (const thePet of pets) {
+                for (const theWalker of walkers) {
+                    if (thePet.walkerId === theWalker.id && thePet.id === parseInt(petId)) {
+                        window.alert(`${thePet.name} is being walked by ${theWalker.name}`)
+                    }
+                }
+            }
+        }
+    }
+)
+
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id=pet--${pet.id}>${pet.name}</li>`
     }
 
     petHTML += "</ul>"
